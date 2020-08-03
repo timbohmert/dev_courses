0e759cbd3edb806a8556c2dd01bf302298d029be
Tim Bohmert <bohm0050@gmail.com>
2020-08-02 23:12:32 -0500

    Completed lodash library project by Codecademy. Project that recreated methods of the lodash library.

--- Codecademy/lodash_library/_.js
+++ Codecademy/lodash_library/_.js
@@ -32,17 +32,71 @@ const _ = {
         return start <= num && num < end;
     },
 
-    //words method. For loops 
+    //words method. splits string into array of words
     words(str) {
-
+        words = str.split(' ')
+        return words
     },
 
     //.pad() method 
     pad(str, length) {
-
+        pad = length - str.length
+        while (pad > 0) {
+            if  (pad > 1) {
+                str = ' ' + str + ' ';
+                pad -= 2;
+            } else {
+                str = str + ' ';
+                pad -= 1;
+            }
+        } return str;
     },
 
-    //
+    //.has() method to check if key has value as direct property of object
+    has(obj, key) {
+        return obj[key] !== undefined;
+    },
+
+    //.invert() method creates array object composed of inverted keys and values of object
+    invert(obj) {
+        new_obj = {};
+        for (const old_key in obj) {
+            new_key = obj[old_key];
+            new_obj[new_key] = old_key;
+        } return new_obj;
+    },
+
+    //.findKey() method returns key of first element predicate returns truthy for - predicate is function that returns boolean
+    findKey(obj, predicate) {
+        for (key in obj) {
+            value = obj[key]; 
+            if (predicate(value)) {
+                return key;
+            }
+        }
+    },
+
+    //.drop() method creates slice of array with n elements dropped from beginning
+    drop(arr, n = 1) {
+        return arr.slice(n, arr.length);
+    },
+
+    //.dropWhile() method creates slice of array excluding elements dropped from beginning. elements are dropped until predicate returns falsey
+    dropWhile(arr, predicate) {
+        i = 0
+        while (predicate(arr[i], i, arr)) {
+            i++;
+        } return this.drop(arr, i);
+    },
+
+    //.chunk() method creates array of elements split into groups the length of size, if can't split evenly, final chunk will be remaining elements
+    chunk(arr, n = 1) {
+        new_arr = [];
+        for (i=0; i < arr.length; i += n) {
+            new_arr.push(arr.slice(i, i + n));
+        } return new_arr;
+    }
+
 };
 
 
@@ -52,3 +106,5 @@ const _ = {
 
 // Do not write or modify code below this line.
 module.exports = _;
+
+console.log(_.words('Hello my name is tim.'))
