08cc745c27e4be0bba3c6ed36da9c60b9bd4f6c9
Tim Bohmert <bohm0050@gmail.com>
2020-08-20 23:29:58 -0500

    created file system for codecademy project a_sorted_tale which is project on sorting algos

---
+++ Codecademy/a_sorted_tale/script.py
@@ -0,0 +1,14 @@
+#Goal (supplied by codecademy): You recently began employment at “A Sorted Tale”, an independent bookshop. Every morning, the owner decides to sort the books in a new way.
+
+
+    #Some of his favorite methods include:
+    #By author name
+    #By title
+    #By number of characters in the title
+    #By the reverse of the author’s name
+
+    #Throughout the day, patrons of the bookshop remove books from the shelf. Given the strange ordering of the store, they do not always get the books placed back in exactly the correct location.
+
+    #The owner wants you to research methods of fixing the book ordering throughout the day and sorting the books in the morning. It is currently taking too long!
+
+import utils, sorts
---
+++ Codecademy/a_sorted_tale/utils.py
@@ -0,0 +1,15 @@
+#supplied by codecademy
+
+import csv
+
+# This code loads the current book
+# shelf data from the csv file
+def load_books(filename):
+  bookshelf = []
+  with open(filename) as file:
+      shelf = csv.DictReader(file)
+      for book in shelf:
+          # add your code here
+          
+          bookshelf.append(book)
+  return bookshelf
