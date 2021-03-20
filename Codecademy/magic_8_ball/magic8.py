34442053b76b979cd49acd27f1741ee397fbbb0c
Tim Bohmert <bohm0050@gmail.com>
2021-03-20 14:25:14 -0500

    Completed Magic 8 Ball project on Codecademy

---
+++ Codecademy/magic_8_ball/magic8.py
@@ -0,0 +1,44 @@
+import random
+
+name = input('What is your name? ')
+
+question = input('What question do you have for the Magic 8-Ball? ')
+
+answer = ''
+
+random_number = random.randint(1, 9)
+
+# print(random_number)
+
+if random_number == 1:
+    answer = "Yes - definitely"
+elif random_number == 2:
+    answer = "It is decidedly so"
+elif random_number == 3:
+    answer = 'Without a doubt'
+elif random_number == 4:
+    answer = "Reply hazy, try again"
+elif random_number == 5:
+    answer = 'Ask again later'
+elif random_number == 6:
+    answer = "Better not tell you now"
+elif random_number == 7:
+    answer = 'My sources say no'
+elif random_number == 8:
+    answer = 'Outlook not so good'
+elif random_number == 9:
+    answer = 'Very doubtful'
+else:
+    answer = 'Error'
+
+if (question):
+    if (name):
+        print('{0} asks: {1}'.format(name, question))
+    else:
+        print('Question: ' + question)
+
+    print('Magic 8-ball\'s answer: ' + answer)
+else:
+    print('Please feed me a question!')
+
+
